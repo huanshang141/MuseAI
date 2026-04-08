cfd82d0ff927f75b3c473461cb08afe9cc22a5af
singer141 <1375436987@qq.com>
2026-04-08 20:05:41 +0800

    fix: 修复管理前端的重复侧边栏问题

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,45 +1,15 @@
 <script setup>
-import { Collection, MapLocation, Document } from '@element-plus/icons-vue'
+// Admin view - navigation is handled by AppSidebar.vue
 </script>
 
 <template>
   <div class="admin-view">
-    <el-container>
-      <el-aside width="200px">
-        <el-menu
-          :default-active="$route.path"
-          router
-          class="admin-menu"
-        >
-          <el-menu-item index="/admin/exhibits">
-            <el-icon><Collection /></el-icon>
-            <span>展品管理</span>
-          </el-menu-item>
-          <el-menu-item index="/admin/tour-paths">
-            <el-icon><MapLocation /></el-icon>
-            <span>路线管理</span>
-          </el-menu-item>
-          <el-menu-item index="/admin/prompts">
-            <el-icon><Document /></el-icon>
-            <span>提示词管理</span>
-          </el-menu-item>
-        </el-menu>
-      </el-aside>
-
-      <el-main>
-        <router-view />
-      </el-main>
-    </el-container>
+    <router-view />
   </div>
 </template>
 
 <style scoped>
 .admin-view {
   height: 100%;
-}
-
-.admin-menu {
-  height: 100%;
-  border-right: none;
 }
 </style>
